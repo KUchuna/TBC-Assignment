9f47bc017bf3cb0e0bfc63ba9ce8682272f8f07b
KUchuna <103968053+KUchuna@users.noreply.github.com>
2024-01-20 01:57:44 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const header = document.getElementById('header')
+
+let scrollPoisiton = window.scrollY
+
+//gets the current scroll position and changes opacity for header element
+window.addEventListener('scroll', () => {
+    scrollPoisiton = window.scrollY
+
+    if(scrollPoisiton > 0) {
+        header.classList.add('toggle-opacity')
+    }else {
+        header.classList.remove('toggle-opacity')
+    }
+    console.log(scrollPoisiton)
+})
+
