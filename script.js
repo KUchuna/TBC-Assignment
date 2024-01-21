672219ac23d969968dc9ba447fb8c98462022533
KUchuna <103968053+KUchuna@users.noreply.github.com>
2024-01-21 22:40:51 +0400

    Added sponsors section

--- script.js
+++ script.js
@@ -11,5 +11,86 @@ window.addEventListener('scroll', () => {
     }else {
         header.classList.remove('toggle-opacity')
     }
-    console.log(scrollPoisiton)
+})
+
+
+
+// this portion is for sponsors page
+
+const prevArrow = document.getElementById('prev-arrow')
+const nextArrow = document.getElementById('next-arrow')
+const pages = document.querySelectorAll('.sponsor-page')
+const dotsContainer = document.getElementById('dots-container')
+const dots = document.querySelectorAll('.sponsor-dot')
+
+let currentPage = 0;
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    // shows the selected page by changing its opacity
+
+    function showPage(index) {
+        pages.forEach((page, i) => {
+            page.style.opacity = (i === index) ? 1 : 0;
+        });
+        
+        updateDots(index)
+    }
+
+    // manually change sponsors page with arrows
+    
+    prevArrow.addEventListener('click', function () {
+        currentPage = (currentPage - 1 + pages.length) % pages.length;
+        showPage(currentPage);
+    });
+
+    
+    nextArrow.addEventListener('click', function () {
+        currentPage = (currentPage + 1) % pages.length;
+        showPage(currentPage);
+    });
+
+    dots.forEach((dot, index) => {
+        dot.addEventListener('click', function () {
+            currentPage = index;
+            showPage(currentPage);
+        });
+    });
+    
+    function updateDots(index) {
+        dots.forEach((dot, i) => {
+            dot.classList.toggle('active', i === index);
+        });
+    }
+
+    //timer for the pages to change automatically
+
+    function startTimer() {
+        timer = setInterval(function () {
+            currentPage = (currentPage + 1) % pages.length;
+            showPage(currentPage);
+        }, 5000);
+    }
+
+    function stopTimer() {
+        clearInterval(timer);
+    }
+
+    //this is needed so that if user is hovering/clicking on the arrows or dots
+    //the timer will stop, so that it does not double skip the pages
+    //and then the timer starts again once the user moves mouse away
+
+    prevArrow.addEventListener('mouseenter', stopTimer)
+    prevArrow.addEventListener('mouseleave', startTimer)
+    
+    nextArrow.addEventListener('mouseenter', stopTimer)
+    nextArrow.addEventListener('mouseleave', startTimer)
+
+    dotsContainer.addEventListener('mouseenter', stopTimer)
+    dotsContainer.addEventListener('mouseleave', startTimer)
+
+
+    startTimer();
+    showPage(currentPage);
 })
